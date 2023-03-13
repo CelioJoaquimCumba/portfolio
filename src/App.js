528a5147d28d758c4b3ebe8b269c0ce519507070
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-03-14 01:36:39 +0200

    feat: make navBar for smaller screens

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.scss';
 import Navbar from './components/navbar/Navbar';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,46 @@
 import './Navbar.scss'
+import { NavDropdown } from 'react-bootstrap'
 function Navbar() {
-    return ( 
-        <nav className="nav-bar">
-            <p className="nav-title">Celio Cumba</p>
-            <ul className="nav-items">
-                <li className="nav-item">Summary</li>
-                <li className="nav-item">About me</li>
-                <li className="nav-item">Personal</li>
-                <li className="nav-item">Professional</li>
-                <li className="nav-item">Contact me</li>
-            </ul>
-            <button className="nav-button">Resume</button>
-        </nav>
-     );
+    return (
+      <nav className="nav-bar">
+        <p className="nav-title">Celio Cumba</p>
+        <ul className="nav-items">
+          {[
+            "summary",
+            "about me",
+            "personal",
+            "professional",
+            "contact  me",
+          ].map((item, index) => (
+            <li className="nav-item" key={index}>
+              <a className='nav-link' href={`./#${item}`}>{item}</a>
+            </li>
+          ))}
+        </ul>
+        <button className="nav-button">Resume</button>
+        <NavDropdown className="nav-menu" title="Menu" id="basic-nav-dropdown">
+          <NavDropdown.Item className="nav-item" href="./#summary">
+            Summary
+          </NavDropdown.Item>
+          <NavDropdown.Item className="nav-item" href="./#about">
+            About me
+          </NavDropdown.Item>
+          <NavDropdown.Item className="nav-item" href="./#personal">
+            Personal
+          </NavDropdown.Item>
+          <NavDropdown.Item className="nav-item" href="./#professional">
+            Professional
+          </NavDropdown.Item>
+          <NavDropdown.Item className="nav-item" href="./#contact">
+            Contact me
+          </NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item className="nav-item" href="/resume">
+            Resume
+          </NavDropdown.Item>
+        </NavDropdown>
+      </nav>
+    );
 }
 
 export default Navbar;
