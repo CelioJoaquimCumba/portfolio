72ae85edb04b9ace62080a18e253912cf1c98356
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-03-14 10:26:10 +0200

    feat: add main component to app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Navbar from './components/navbar/Navbar';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import Main from './components/main/Main';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Main/>
     </div>
   );
 }
