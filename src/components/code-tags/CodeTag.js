fc60cc0cc8cf0f6cdccfe4f902c1c635e75905ee
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-03-14 16:31:38 +0200

    refactor: had alternative for wrong props

--- src/components/code-tags/CodeTag.js
+++ src/components/code-tags/CodeTag.js
@@ -1,12 +1,17 @@
 import './CodeTag.scss'
-function CodeTag({ children, tag }) {
-  return (
-    <div className="code-tag">
-      <p>{`<${tag}>`}</p>
-      <div className="code-tag-content">{children}</div>
-      <p>{`</${tag}>`}</p>
-    </div>
-  );
+function CodeTag({ children, tag, className }) {
+    if (tag) {
+    return (
+      <div className={`code-tag`}>
+        <p>{`<${tag}>`}</p>
+        <div className={`code-tag-content ${className}`}>{children}</div>
+        <p>{`</${tag}>`}</p>
+      </div>
+    );
+
+    } else {
+        return <>{children}</>
+    }
 }
 
 export default CodeTag;
