009beefac228b041408ba19fad7cf409a1d55cf7
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-03-14 10:25:42 +0200

    feat: add main component

---
+++ src/components/main/Main.js
@@ -0,0 +1,13 @@
+import CodeTag from "../code-tags/CodeTag";
+import './Main.scss'
+function Main() {
+    return ( 
+        <main className="main">
+            <CodeTag tag="main">
+                hello
+            </CodeTag>
+        </main>
+     );
+}
+
+export default Main;
