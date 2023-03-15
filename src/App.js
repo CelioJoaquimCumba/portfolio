5c23962e282e2b041105f436e8b1c566dc715465
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-03-15 08:02:10 +0200

    feat: add header component to the app component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import Main from './components/main/Main';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Header/>
       <Main/>
     </div>
   );
