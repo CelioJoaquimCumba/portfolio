d1422d76290b1408fbfc6028e2403e510d599bf0
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-03-15 23:02:19 +0200

    feat: add about section to main.js

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,16 +1,21 @@
 import CodeTag from "../code-tags/CodeTag";
 import './Main.scss'
+import About from "./sections/about/About";
 import Summary from "./sections/summary/Summary";
 function Main() {
-    return ( 
-        <main>
-            <CodeTag tag="main">
-                <CodeTag tag={'section'} idAtr={'summary'}>
-                    <Summary/>
-                </CodeTag>
-            </CodeTag>
-        </main>
-     );
+    return (
+      <main>
+        <CodeTag tag="main">
+          <CodeTag tag={"section"} idAtr={"summary"}>
+            <Summary />
+          </CodeTag>
+
+          <CodeTag tag={"section"} idAtr={"about"}>
+            <About/>
+          </CodeTag>
+        </CodeTag>
+      </main>
+    );
 }
 
 export default Main;
