7363a13e57d67993137d8258cfb5b11b43947f39
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-03-17 13:17:04 +0200

    feat: add structure and style to goal component

--- src/components/main/sections/about/parts/goals/Goals.js
+++ src/components/main/sections/about/parts/goals/Goals.js
@@ -1,8 +1,20 @@
 import './Goals.scss'
 function Goals() {
-    return ( 
-        <p>Goals</p>
-     );
+    return (
+      <div className="goal-section" id="goal">
+        <div className="goal-container">
+          <h1 className="goal-header">Goal</h1>
+          <p className="goal-description">
+            <b className="goal-highlight">I want to CHANGE the world.</b>My goal
+            is to make a positive impact on the world by utilizing the skills I
+            am developing. I aspire to leave behind a better world for future
+            generations by leveraging technology to address the major challenges
+            we face. I am committed to doing everything in my power to eradicate
+            these problems and create a brighter future for all.
+          </p>
+        </div>
+      </div>
+    );
 }
 
 export default Goals;
