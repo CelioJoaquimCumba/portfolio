58a5dc55d3878c499f86e2c174806dafa48d7c31
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-03-17 15:03:05 +0200

    feat: add footer component to app component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 import Main from './components/main/Main';
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Header/>
       <hr className='divider'/>
       <Main/>
+      <Footer/>
     </div>
   );
 }
