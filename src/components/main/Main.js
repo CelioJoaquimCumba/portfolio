85b47afb14e38abd1e51432102a20a40094d2213
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-03-17 14:19:01 +0200

    feat: add contact component to main component

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,7 @@
 import CodeTag from "../code-tags/CodeTag";
 import './Main.scss'
 import About from "./sections/about/About";
+import Contact from "./sections/contact/Contact";
 import Summary from "./sections/summary/Summary";
 function Main() {
     return (
@@ -11,7 +12,10 @@ function Main() {
           </CodeTag>
 
           <CodeTag tag={"section"} idAtr={"about"}>
-            <About/>
+            <About />
+          </CodeTag>
+          <CodeTag tag={"section"} idAtr={"contact"}>
+            <Contact />
           </CodeTag>
         </CodeTag>
       </main>
