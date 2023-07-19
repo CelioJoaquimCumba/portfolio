b673679da54de7c93055c74e8ff3fa2f4beee9ef
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2023-07-19 13:42:47 +0200

    feat: add hover effect in certifications button

--- src/components/main/sections/about/About.js
+++ src/components/main/sections/about/About.js
@@ -2,8 +2,8 @@ import './About.scss';
 import Skills from './parts/skills/Skills';
 import Education from './parts/education/Education';
 import Certifications from './parts/certifications/Certifications';
-import Projects from './parts/projects/Projects';
 import Goals from './parts/goals/Goals';
+import Projects from './parts/projects/Projects';
 function About() {
     return (
       <div id="about">
